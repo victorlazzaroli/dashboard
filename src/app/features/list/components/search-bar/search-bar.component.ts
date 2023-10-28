4ac1a4cc260092589afcd44d651d22f4d9cec219
Victor <the_wizard_ita@hotmail.it>
2023-10-28 21:11:48 +0200

    list feature - search bar component UI

---
+++ src/app/features/list/components/search-bar/search-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent {
+  value: string | null = '';
+
+  searchEvnt(value: string) {
+
+  }
+}
